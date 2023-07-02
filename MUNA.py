9e480362021fb38fad818eeee4f02917621711da
ＨｅｎｄＭｕｎ <88487729+muniroh96@users.noreply.github.com>
2023-07-02 20:06:34 +0700

    Add files via upload

---
+++ MUNA.py
@@ -0,0 +1,8 @@
+import os
+print("[✓] JOIN MHB CHAT GROUP")
+os.system("xdg-open https://chat.whatsapp.com/DTFRXJF2wop3moMbyUPKrm")
+import os
+print("[✓] PLEASE WAIT CHACKING FOR NEW UPDATE")
+os.system(" git pull")
+import MHB
+
